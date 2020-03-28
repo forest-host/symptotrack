7e3523f052a932e488802ce376520cd1d7edc789
zeepsopje <stefan@gohike.nl>
2020-03-28 17:00:23 +0100

    add map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Styling
 import { Box } from '../styles';
+import SMyLocation from './styles';
 
 const SymptoMap = () => {
-  return <Box id="map" width={1} css={{ height: '370px' }} />;
+  const [renderMap, setMap] = useState(null);
+  const [position, setPosition] = useState([51.505, -0.09]);
+
+  const getMap = async () => {
+    const { Map, Marker, TileLayer } = require('react-leaflet');
+
+    const map = (
+      <Map center={position} zoom={12}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {position && (
+          <Marker
+            position={position}
+            onDragend={({
+              target: {
+                _latlng: { lat, lng },
+              },
+            }) => setPosition([lat, lng])}
+            draggable
+          />
+        )}
+      </Map>
+    );
+
+    setMap(map);
+  };
+
+  useEffect(() => {
+    if (position) {
+      getMap();
+    }
+  }, [position]);
+
+  return (
+    <Box as={SMyLocation} id="map" width={1} css={{ height: '500px' }}>
+      {renderMap}
+    </Box>
+  );
 };
 
 export default SymptoMap;
--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -5,7 +5,7 @@ const SMyLocation = styled.div`
   position: relative;
 
   .leaflet-container {
-    height: ${px2rem(250)};
+    height: ${px2rem(500)};
   }
 `;
 
