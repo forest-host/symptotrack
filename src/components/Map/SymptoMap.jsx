b29c7eb4fc897a5a07972a9c373cfd94a2058dd4
Stef Winterswijk <stef_winterswijk@hotmail.com>
2020-04-01 14:28:40 +0200

    Tweak map

--- src/components/Map/SymptoMap.jsx
+++ src/components/Map/SymptoMap.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import Router from 'next/router';
 import ReactDOMServer from 'react-dom/server';
 import uuid from 'uuid';
 
@@ -11,7 +13,7 @@ import MarkerIcon from './MarkerIcon';
 // Styling
 import SMap from './styles';
 
-const SymptoMap = () => {
+const SymptoMap = ({ coordinates }) => {
   // Leaflet
   const L = require('leaflet');
   const { Map, Marker, TileLayer } = require('react-leaflet');
@@ -19,14 +21,14 @@ const SymptoMap = () => {
   const mapRef = useRef(null);
   const [data, setData] = useState(null);
 
-  const getData = async ({ zoom, top, right, bottom, left }) => {
+  const getData = async ({ z, top, right, bottom, left }) => {
     await get('data/spots', {
       params: {
-        z: zoom,
-        top,
-        right,
-        bottom,
-        left,
+        z: parseFloat(z),
+        top: parseFloat(top),
+        right: parseFloat(right),
+        bottom: parseFloat(bottom),
+        left: parseFloat(left),
       },
     }).then((resp) => {
       setData(resp);
@@ -34,20 +36,20 @@ const SymptoMap = () => {
   };
 
   useEffect(() => {
-    getData({
-      zoom: 8,
-      top: 54.4,
-      left: -1.22,
-      bottom: 51,
-      right: 14.6,
-    });
+    getData(coordinates);
   }, []);
 
   const onViewportChanged = ({ zoom = 8 }) => {
     const { _southWest, _northEast } = mapRef?.current?.leafletElement.getBounds();
 
+    Router.push(
+      `/map?test=true&z=${zoom}&top=${_northEast?.lat}&right=${_northEast?.lng}&bottom=${_southWest?.lat}&left=${_southWest?.lng}`,
+      `/map?test=true&z=${zoom}&top=${_northEast?.lat}&right=${_northEast?.lng}&bottom=${_southWest?.lat}&left=${_southWest?.lng}`,
+      { shallow: true }
+    );
+
     getData({
-      zoom,
+      z: zoom,
       top: _northEast?.lat,
       right: _northEast?.lng,
       bottom: _southWest?.lat,
@@ -61,13 +63,20 @@ const SymptoMap = () => {
       html: ReactDOMServer.renderToStaticMarkup(<MarkerIcon {...details} />),
     });
 
+  const calculateCenter = ({ top, right, bottom, left }) => {
+    const y = (parseFloat(bottom) + parseFloat(top)) / 2;
+    const x = (parseFloat(right) + parseFloat(left)) / 2;
+
+    return [y, x];
+  };
+
   return (
     <SMap width={1}>
       <Map
-        center={[52.2, 5.8]}
+        center={calculateCenter(coordinates)}
         maxZoom={18}
         minZoom={2}
-        zoom={8}
+        zoom={coordinates.z}
         ref={mapRef}
         onViewportChanged={(e) => onViewportChanged(e)}
       >
@@ -83,6 +92,16 @@ const SymptoMap = () => {
       </Map>
     </SMap>
   );
+};
+
+SymptoMap.propTypes = {
+  coordinates: PropTypes.shape({
+    z: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    top: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    left: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    bottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    right: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  }).isRequired,
 };
 
 export default SymptoMap;
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -13,24 +13,37 @@ import { Container } from '../components/styles';
 
 const SymptoMap = dynamic(() => import('../components/Map'), { ssr: false });
 
-const Map = ({ t, test }) => (
+const Map = ({ t, test, z, top, right, bottom, left }) => (
   <>
     <Container pt={[20, 40]} pb={70} relative>
       {!test && <Hero title={t('map:title')} content={t('map:content')} />}
       {!test && <img src="/static/avatar.jpg" alt="SymptomenKaart" />}
     </Container>
-    {test && <SymptoMap />}
+    {test && <SymptoMap coordinates={{ z, top, right, bottom, left }} />}
   </>
 );
 
 Map.propTypes = {
   t: PropTypes.func.isRequired,
+  z: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  top: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  right: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  bottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  left: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+Map.defaultProps = {
+  z: 8,
+  top: 54.4,
+  left: -1.22,
+  bottom: 50,
+  right: 13,
 };
 
 Map.getInitialProps = async (ctx) => {
-  const { test } = ctx.query;
+  const { test, z, top, right, bottom, left } = ctx.query;
 
-  return { test, namespacesRequired: ['common', 'map', 'socials'] };
+  return { test, z, top, right, bottom, left, namespacesRequired: ['common', 'map', 'socials'] };
 };
 
 export default withTranslation('map')(Map);
